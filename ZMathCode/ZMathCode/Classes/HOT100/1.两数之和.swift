ab50908810aa3559fb6955d280e76037460d502e
zmfflying <zmfflying@qq.com>
2020-06-23 12:00:11 +0800

    两数相加

--- ZMathCode/ZMathCode/Classes/HOT100/1.两数之和.swift
+++ ZMathCode/ZMathCode/Classes/HOT100/1.两数之和.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 zmfflying. All rights reserved.
 //
 /**
+## 题目
  给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
 
  你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
@@ -22,13 +23,17 @@
  来源：力扣（LeetCode）
  链接：https://leetcode-cn.com/problems/two-sum
  著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
- 
- 测试代码:
+
+## 测试代码
  print("\(twoSum([2, 7, 11, 15], 9))")
- 
- 笔记:
+
+## 笔记
  核心就是字典记录之前的数字和下标，之后遍历查询字典里有没有对应的想要的那个数字就行
+
+## 代码地址
+ https://github.com/zmfflying/ZMathCode
  */
+
 import Foundation
 
 func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
